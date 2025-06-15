a61403bda74e87486508fe582fd046b0213228a9
Rochan Madhusudhana <rochana.hm@gmail.com>
2025-06-15 16:20:06 -0700

    create logs if not exist

--- log.py
+++ log.py
@@ -5,6 +5,9 @@ load_dotenv()
 import logging
 import os
 
+# Create logs directory if it doesn't exist
+os.makedirs(os.path.join(os.path.dirname(__file__), "logs"), exist_ok=True)
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     filename=os.path.join(os.path.dirname(__file__), "logs", "app.log"),
